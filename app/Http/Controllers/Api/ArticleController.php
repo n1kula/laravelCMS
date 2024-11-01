be4eb6098faab0a0d8411bbbe58bf4905a5a2bdb
Tomasz <n1kula@wp.pl>
2024-11-01 22:14:50 +0100

    json for article

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,9 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Article;
+use Illuminate\Http\JsonResponse;
 
 class ArticleController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $articles = Article::all()->map(function ($article) {
+            return [
+                'title' => $article->title,
+                'author' => [
+                    'name' => $article->author,
+                    'profileLink' => '/about'
+                ],
+                'date' => $article->date->format('d-m-Y'),
+                'readingTime' => $article->reading_time,
+                'tag' => $article->tag,
+                'image' => [
+                    'link' => $article->image_link,
+                    'alt' => $article->image_alt
+                ],
+                'content' => $article->content
+            ];
+        });
+
+        return response()->json($articles);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/articles', [App\Http\Controllers\Api\ArticleController::class, 'index']);
