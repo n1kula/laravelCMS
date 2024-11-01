a38825b668eafeef73fa267a7a4bdac49d7a7f28
Tomasz <n1kula@wp.pl>
2024-11-01 20:58:34 +0100

    Articles init

---
+++ database/migrations/2024_11_01_195600_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+// database/migrations/xxxx_xx_xx_xxxxxx_create_articles_table.php
+public function up()
+{
+    Schema::create('articles', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->string('author');
+        $table->date('created_at');
+        $table->date('updated_at');
+        $table->string('reading_time');
+        $table->string('tag');
+        $table->string('image_link');
+        $table->string('image_alt');
+        $table->text('content');
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
