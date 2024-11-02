deac6e13b0b0d319c4d17ae99e58be8cdf517514
Tomasz <n1kula@wp.pl>
2024-11-02 18:31:30 +0100

    article in filament

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -3,54 +3,54 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ArticleResource\Pages;
-use App\Filament\Resources\ArticleResource\RelationManagers;
 use App\Models\Article;
 use Filament\Forms;
-use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ArticleResource extends Resource
 {
     protected static ?string $model = Article::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    // protected static ?string $navigationIcon = 'heroicon-collection';
+    
+    protected static ?string $slug = 'articles';
 
-    public static function form(Form $form): Form
+    protected static ?string $navigationGroup = 'Content Management';
+
+    public static function form(Forms\Form $form): Forms\Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('title')
+                    ->required(),
+                Forms\Components\TextInput::make('author')
+                    ->required(),
+                Forms\Components\DatePicker::make('created_at')
+                    ->required(),
+                Forms\Components\TextInput::make('reading_time')
+                    ->required(),
+                Forms\Components\TextInput::make('tag')
+                    ->required(),
+                Forms\Components\TextInput::make('image_link')
+                    ->required(),
+                Forms\Components\TextInput::make('image_alt')
+                    ->required(),
+                Forms\Components\Textarea::make('content')
+                    ->required(),
             ]);
     }
 
-    public static function table(Table $table): Table
+    public static function table(Tables\Table $table): Tables\Table
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('author'),
+                Tables\Columns\TextColumn::make('created_at'),
+                Tables\Columns\TextColumn::make('reading_time'),
+                Tables\Columns\TextColumn::make('tag'),
             ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
     }
 
     public static function getPages(): array
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'author',
+        'created_at',
+        'updated_at',
+        'reading_time',
+        'tag',
+        'image_link',
+        'image_alt',
+        'content',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 }
